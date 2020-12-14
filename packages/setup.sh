8d90ce568ae7ac3b0dfa8ef13cfd49966a78c662
Brentley Jones <github@brentleyjones.com>
2020-12-14 14:51:04 -0600

    Allow non-critical package install failures

--- packages/setup.sh
+++ packages/setup.sh
@@ -10,47 +10,84 @@ source ../scripts/functions.sh
 if test ! $(which brew); then
     info "installing Homebrew"
 
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null
+    if ! /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null; then
+        error "failed to install Homebrew"
+    fi
 fi
 
 # Install xcodes
 if test ! $(which xcodes); then
     info "installing xcodes"
 
-    brew install robotsandpencils/made/xcodes
+    if ! brew install robotsandpencils/made/xcodes; then
+        error "failed to install xcodes"
+    fi
 fi
 
 # Install Xcode if needed
 if [ "$(xcode-select -p)" == "/Library/Developer/CommandLineTools" ]; then
     info "installing Xcode"
 
-    xcodes install --latest
-    xcode_path = "$(mdfind kind:application -onlyin /Applications -name 'Xcode-' | tail -n 1)"
-    sudo xcode-select -s "$xcode_path"
+    if ! xcodes install --latest; then
+        error "failed to install latest Xcode"
+    fi
+
+    xcode_path="$(mdfind kind:application -onlyin /Applications -name 'Xcode-' | tail -n 1)"
+    if ! sudo xcode-select -s "$xcode_path"; then
+        error "failed to set xcode-select to latest Xcode"
+    fi
+
     sudo xcodebuild -runFirstLaunch
 fi
 
-# Ensure we are signed into mas
-if test ! $(which mas); then
-    brew install mas
-fi
-
-if ! mas account > /dev/null; then
-    substep_info "signing into the Mac App Store"
-    substep_user ' - What is your Apple ID?'
-    read -e apple_id
-    if mas signin $apple_id; then
-        substep_success "signed into the Mac App Store"
-    else
-        substep_error "failed to sign into the Mac App Store"
-    fi
-fi
-
-info "installing Brewfile packages"
-if brew bundle; then
-    success "finished installing Brewfile packages"
+info "installing required Homebrew packages"
+if brew bundle --file Brewfile-required; then
+    success "finished installing required Homebrew packages"
 else
-    error "failed to install Brewfile packages"
+    error "failed to install required Homebrew packages"
+fi
+
+install_mas_packages() {
+    # Ensure we are signed into mas
+    if ! mas account > /dev/null; then
+        substep_info "signing into the Mac App Store"
+        substep_user ' - What is your Apple ID?'
+        read -e apple_id
+        if mas signin $apple_id; then
+            substep_success "signed into the Mac App Store"
+        else
+            substep_error "failed to sign into the Mac App Store"
+            return 2
+        fi
+    fi
+
+    # Install the packages
+    if ! brew bundle --file Brewfile-mas; then
+        substep_error "failed to install Brewfile-mas packages"
+        return 3
+    fi
+}
+
+info "installing Mac App Store apps"
+if install_mas_packages; then
+    success "finished installing Mac App Store apps"
+else
+    error "failed to install Mac App Store apps" false
+fi
+
+install_other_brew_packages() {
+    if brew bundle --file Brewfile-required; then
+        substep_success "finished installing remaining Brewfile packages"
+    else
+        substep_error "failed to install remaining Brewfile packages"
+    fi
+}
+
+info "installing remaining Homebrew packages"
+if brew bundle --file Brewfile-required; then
+    success "finished installing remaining Homebrew packages"
+else
+    error "failed to install remaining Homebrew packages" false
 fi
 
 find * -name "*.list" | while read fn; do
@@ -62,7 +99,9 @@ find * -name "*.list" | while read fn; do
             continue
         fi
         substep_info "installing $package"
-        $cmd $package
+        if ! $cmd $package; then
+            substep_error "failed to install $package"
+        fi
     done < "$fn"
     success "finished installing $1 packages"
 done
