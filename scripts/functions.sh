9253e9c2cb88a215fbe1b2159a0b54f2b8adbd8f
Brentley Jones <github@brentleyjones.com>
2021-04-19 14:26:04 -0500

    Remove unused helper function

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -126,26 +126,3 @@ clear_broken_symlinks() {
         fi
     done
 }
-
-# Took these printing functions from https://github.com/Sajjadhosn/dotfiles
-coloredEcho() {
-    local exp="$1";
-    local color="$2";
-    local arrow="$3";
-    if ! [[ $color =~ '^[0-9]$' ]] ; then
-       case $(echo $color | tr '[:upper:]' '[:lower:]') in
-        black) color=0 ;;
-        red) color=1 ;;
-        green) color=2 ;;
-        yellow) color=3 ;;
-        blue) color=4 ;;
-        magenta) color=5 ;;
-        cyan) color=6 ;;
-        white|*) color=7 ;; # white or invalid color
-       esac
-    fi
-    tput bold;
-    tput setaf "$color";
-    echo "$arrow $exp";
-    tput sgr0;
-}
