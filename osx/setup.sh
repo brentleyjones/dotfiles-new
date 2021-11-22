8c7a90444ac082a7c6375be96c8d218629dd11b7
Brentley Jones <github@brentleyjones.com>
2021-11-22 10:49:05 -0600

    Move dock setup to prevent race condition

--- osx/setup.sh
+++ osx/setup.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 cd "${BASH_SOURCE[0]%/*}" || exit 1
 
-# Defaults for the system and all apps
-./defaults.sh
-
 # Setup finder sidebar
 mkdir -p ~/Developer
 ./edit_sidebar.py
 
 # Setup dock
 ./dock.sh
+
+# Defaults for the system and all apps
+./defaults.sh
